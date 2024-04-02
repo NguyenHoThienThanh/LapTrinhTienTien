88ef65782ddd80d0df53365152f5c07ff70816e4
NguyenHoThienThanh <nguyenhothienthanh26122003@gmail.com>
2024-04-02 10:13:52 +0700

    IQuanHeDAO

---
+++ DoAn_LTTT_Nhom13/src/InterfaceDAO/IQuanHeDAO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package InterfaceDAO;
+
+import Models.QuanHeModel;
+import java.util.List;
+
+/**
+ *
+ * @author TUAN
+ */
+public interface IQuanHeDAO {
+    List<QuanHeModel> findAll();
+    QuanHeModel findOneByMaHK (String MaHK);
+    boolean insert (QuanHeModel quanHe);
+    boolean update (QuanHeModel quanHe);
+    boolean delete (String MaHK);
+}
