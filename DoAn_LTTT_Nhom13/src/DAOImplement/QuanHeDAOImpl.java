af7fbe7f6f23867b33a5ba262780d1a9defb02f7
NguyenHoThienThanh <nguyenhothienthanh26122003@gmail.com>
2024-04-02 10:13:14 +0700

    CRUD QuanHe

---
+++ DoAn_LTTT_Nhom13/src/DAOImplement/QuanHeDAOImpl.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAOImplement;
+
+import InterfaceDAO.DBConnection;
+import InterfaceDAO.IQuanHeDAO;
+import Models.QuanHeModel;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author TUAN
+ */
+public class QuanHeDAOImpl implements IQuanHeDAO{
+Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    
+    @Override
+    public List<QuanHeModel> findAll() {
+        String query = "SELECT * FROM DangNhap WHERE TrangThai = 1 " ;
+        List<QuanHeModel> listQuanHe = new ArrayList<>();
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                QuanHeModel quanHe = new QuanHeModel();
+                quanHe.setMaHK(rs.getString(1));
+                quanHe.setKhaiSinhNguoiThamGia(rs.getString(2));
+                quanHe.setQuanHeVoiChuHo(rs.getString(3));
+                quanHe.setTrangThai(rs.getInt(4));
+                listQuanHe.add(quanHe);
+            }
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listQuanHe;
+    }
+
+    @Override
+    public QuanHeModel findOneByMaHK(String MaHK) {
+        String query = "SELECT * FROM DangNhap WHERE MaHK =? " ;
+        QuanHeModel quanHe = new QuanHeModel();
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, MaHK);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                quanHe.setMaHK(rs.getString(1));
+                quanHe.setKhaiSinhNguoiThamGia(rs.getString(2));
+                quanHe.setQuanHeVoiChuHo(rs.getString(3));
+                quanHe.setTrangThai(rs.getInt(4));
+            }
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return quanHe;
+    }
+
+    @Override
+    public boolean insert(QuanHeModel quanHe) {
+        String query = "INSERT INTO QuanHe (MaHK, KhaiSinhNguoiThamGia, QuanHeVoiChuHo, TrangThai) VALUES (?,?,?,?)";
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, quanHe.getMaHK());
+            ps.setString(2, quanHe.getKhaiSinhNguoiThamGia());
+            ps.setString(3, quanHe.getQuanHeVoiChuHo());
+            ps.setInt(4, quanHe.getTrangThai());
+            ps.executeUpdate();
+            conn.close();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean update(QuanHeModel quanHe) {
+        String query = "UPDATE QuanHe SET MaHK =?, KhaiSinhNguoiThamGia=?, QuanHeVoiChuHo=?, TrangThai=?";
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, quanHe.getMaHK());
+            ps.setString(2, quanHe.getKhaiSinhNguoiThamGia());
+            ps.setString(3, quanHe.getQuanHeVoiChuHo());
+            ps.setInt(4, quanHe.getTrangThai());
+            ps.executeUpdate();
+            conn.close();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean delete(String MaHK) {
+        String query = "DELETE QuanHe WHERE MaHK= ?";
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, MaHK);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+    
+}
