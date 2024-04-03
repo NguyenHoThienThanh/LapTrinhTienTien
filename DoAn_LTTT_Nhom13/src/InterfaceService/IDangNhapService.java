4747461e3e6a4367cbbdb89f14ec036ea1fb7658
DangGiaThuan <136982789+DangGiaThuan@users.noreply.github.com>
2024-04-03 12:04:45 +0700

    Update IDangNhapService.java

--- DoAn_LTTT_Nhom13/src/InterfaceService/IDangNhapService.java
+++ DoAn_LTTT_Nhom13/src/InterfaceService/IDangNhapService.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package InterfaceService;
 
 import Models.DangNhapModel;
 import java.util.List;
 
-/**
- *
- * @author Admin
- */
 public interface IDangNhapService {
     List<DangNhapModel> findAll();
     DangNhapModel findOne(String userName);
