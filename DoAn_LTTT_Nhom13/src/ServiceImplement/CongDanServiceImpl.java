8fb080dab4716f5b8d3f7ead1e3b739a6bc8204c
Lamenth <136982309+NguyenHoThienThanh@users.noreply.github.com>
2024-04-07 12:19:01 +0700

    Add files via upload

--- DoAn_LTTT_Nhom13/src/ServiceImplement/CongDanServiceImpl.java
+++ DoAn_LTTT_Nhom13/src/ServiceImplement/CongDanServiceImpl.java
@@ -1,15 +1,23 @@
-
 package ServiceImplement;
 
 import DAOImplement.CongDanDAOImpl;
+import InterfaceDAO.DBConnection;
 import InterfaceDAO.ICongDanDAO;
 import InterfaceService.ICongDanService;
 import Models.CongDanModel;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
-public class CongDanServiceImpl implements ICongDanService{
+public class CongDanServiceImpl implements ICongDanService {
 
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
     ICongDanDAO congDanDao = new CongDanDAOImpl();
+
     @Override
     public List<CongDanModel> findAll() {
         return congDanDao.findAll();
@@ -42,5 +50,44 @@ public class CongDanServiceImpl implements ICongDanService{
     public boolean delete(String CCCD) {
         return congDanDao.delete(CCCD);
     }
-    
+
+    //Update TrangThai = 1
+    @Override
+    public boolean updateStatus(String CCCD) {
+        String query = "UPDATE CongDan SET TrangThai = 1 WHERE CCCD = ?";
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, CCCD);
+            ps.executeUpdate();
+            conn.close();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    //Check tồn tại số CCCD nào chưa
+    @Override
+    public boolean isCCCDIssued(String MaKS) {
+        String query = "SELECT TrangThai FROM CongDan WHERE MaKS = ?";
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, MaKS);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                int trangThai = rs.getInt("TrangThai");
+                if(trangThai ==1 ){
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            
+        }
+        return false;
+    }
+
 }
