40b6283bdaf07473dfa39eb90c7616d8188d6952
GiaThuanSenpai <142809997+GiaThuanSenpai@users.noreply.github.com>
2024-04-09 10:39:46 +0700

    Update HoKhauServiceImpl.java

--- DoAn_LTTT_Nhom13/src/ServiceImplement/HoKhauServiceImpl.java
+++ DoAn_LTTT_Nhom13/src/ServiceImplement/HoKhauServiceImpl.java
@@ -5,10 +5,14 @@
 package ServiceImplement;
 
 import DAOImplement.HoKhauDAOImpl;
+import InterfaceDAO.DBConnection;
 import InterfaceDAO.IHoKhauDAO;
 import InterfaceService.IHoKhauService;
 import Models.HoKhauModel;
+import Models.ThongTinHoKhau;
+import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
 
 /**
  *
@@ -16,6 +20,10 @@ import java.util.List;
  */
 public class HoKhauServiceImpl implements IHoKhauService{
 
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    
     IHoKhauDAO hoKhauDAO = new HoKhauDAOImpl();
     @Override
     public List<HoKhauModel> findAll() {
@@ -28,8 +36,8 @@ public class HoKhauServiceImpl implements IHoKhauService{
     }
 
     @Override
-    public boolean insert(HoKhauModel hoKhau) {
-        return hoKhauDAO.insert(hoKhau);
+        public boolean insert(HoKhauModel hoKhau) {
+            return hoKhauDAO.insert(hoKhau);
     }
 
     @Override
@@ -47,6 +55,59 @@ public class HoKhauServiceImpl implements IHoKhauService{
     @Override
     public boolean delete(String MaHK) {
         return hoKhauDAO.delete(MaHK);
+    }
+
+    @Override
+    public List<ThongTinHoKhau> findAllHoKhau(String CCCD) {
+        String query = "  select CCCD as CCCDNguoiThan, HoTen as HoTenNguoiThan, SDT, NgaySinh, QuanHeVoiChuHo, MaHK from CongDan join (select MaKS, HoTenKS, GioiTinh, NgaySinh, MaHK, KhaiSinhNguoiThamGia, QuanHeVoiChuHo, DiaChi from KhaiSinh join (select T.MaHK, KhaiSinhNguoiThamGia, QuanHeVoiChuHo, DiaChi from QuanHe join (select MaHK, DiaChi, KhaiSinhChuHo from HoKhau where MaHK in (SELECT MaHK FROM QuanHe join CongDan on QuanHe.KhaiSinhNguoiThamGia = CongDan.MaKS WHERE QuanHe.TrangThai = 1 and CCCD = ?)) T on QuanHe.MaHK = T.MaHK) Q on Q.KhaiSinhNguoiThamGia = KhaiSinh.MaKS) C on C.KhaiSinhNguoiThamGia = CongDan.MaKS";
+        List<ThongTinHoKhau> listThongTinHoKhau = new ArrayList<>();
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, CCCD);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                ThongTinHoKhau tthk = new ThongTinHoKhau();
+                tthk.setCCCDNguoiThan(rs.getString("CCCDNguoiThan"));
+                tthk.setHoTenNguoiThan(rs.getString("HoTenNguoiThan"));
+                tthk.setNgaySinh(rs.getDate("NgaySinh"));
+                tthk.setQuanHeVoiChuHo(rs.getString("QuanHeVoiChuHo"));
+                tthk.setMaHoKhau(rs.getString("MaHK"));
+                listThongTinHoKhau.add(tthk);
+            }
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return listThongTinHoKhau;
+        
+    }
+
+    @Override
+    public ThongTinHoKhau findOneByCCCD(String CCCD) {
+        String query = "  select CCCD as CCCDChuHo, HoTen as HoTenChuHo, MaHK, DiaChi, HoTenNguoiThan as HoTenNguoiDangNhap, CCCDNguoiDangNhap, QuanHeVoiChuHo from CongDan join (select P.MaHK, DiaChi, KhaiSinhChuHo, TrangThai, HoTenKS as HoTenNguoiThan, GioiTinh, CCCD as CCCDNguoiDangNhap, QuanHeVoiChuHo from  HoKhau join (select MaHK, KhaiSinhNguoiThamGia, QuanHeVoiChuHo, HoTenKS, GioiTinh, CCCD from QuanHe join (select T.MaKS, HoTenKS, GioiTinh, CCCD from KhaiSinh join (select * from CongDan where CCCD = ? and TrangThai = 1) T on KhaiSinh.MaKS = T.MaKS) Q on Q.MaKS = QuanHe.KhaiSinhNguoiThamGia)P on P.MaHK = HoKhau.MaHK) R on R.KhaiSinhChuHo = CongDan.MaKS and R.TrangThai = 1";
+        ThongTinHoKhau tthk = new ThongTinHoKhau();
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, CCCD);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                tthk.setCCCDChuHo(rs.getString("CCCDChuHo"));
+                tthk.setHoTenChuHo(rs.getString("HoTenChuHo"));
+                tthk.setMaHoKhau(rs.getString("MaHK"));
+                tthk.setDiaChi(rs.getString("DiaChi"));
+                tthk.setHoTenNguoiDangNhap(rs.getString("HoTenNguoiDangNhap"));
+                tthk.setCCCDNguoiDangNhap(rs.getString("CCCDNguoiDangNhap"));
+                tthk.setQuanHeVoiChuHo(rs.getString("QuanHeVoiChuHo"));
+            }
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tthk;
+        
     }
     
 }
