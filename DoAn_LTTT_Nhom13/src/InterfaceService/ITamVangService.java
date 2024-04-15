b806ca98d96e855b330a16cd06390b3ea2230364
Lamenth <136982309+NguyenHoThienThanh@users.noreply.github.com>
2024-04-15 07:29:47 +0700

    Update ITamVangService.java

--- DoAn_LTTT_Nhom13/src/InterfaceService/ITamVangService.java
+++ DoAn_LTTT_Nhom13/src/InterfaceService/ITamVangService.java
@@ -21,5 +21,6 @@ public interface ITamVangService {
     boolean update (TamVangModel tamVang);
     boolean delete (String MaTV);
     List<DonTamVang> findAllTV(String CCCD);
+    int ifExists(String CCCD);
     List<DonTamVangUser> findAllTVuser(String CCCD);    
 }
