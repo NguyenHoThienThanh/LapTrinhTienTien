cb6a2df6786144b87a06d5c56b6d91f4cde71642
21110512@st.hcmute.edu.vn <21110512@st.hcmute.edu.vn>
2024-05-16 10:31:57 +0700

    fix chart

--- DoAn_LTTT_Nhom13/src/Controller/Admin/ChartKetHonController.java
+++ DoAn_LTTT_Nhom13/src/Controller/Admin/ChartKetHonController.java
@@ -64,7 +64,7 @@ public class ChartKetHonController extends javax.swing.JPanel {
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Biểu đồ thống kê kết hôn theo từng năm ");
 
-        cbx_nam.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024" }));
+        cbx_nam.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "2023", "2024" }));
         cbx_nam.setLabeText("");
         cbx_nam.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
--- DoAn_LTTT_Nhom13/src/Controller/Admin/ChartKhaiSinhController.java
+++ DoAn_LTTT_Nhom13/src/Controller/Admin/ChartKhaiSinhController.java
@@ -61,8 +61,7 @@ public class ChartKhaiSinhController extends javax.swing.JPanel {
 
         jPanel1.setBackground(new java.awt.Color(34, 59, 69));
 
-        cbx_nam.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024" }));
-        cbx_nam.setSelectedIndex(3);
+        cbx_nam.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "2023", "2024" }));
         cbx_nam.setLabeText("");
         cbx_nam.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
